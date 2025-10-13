ea297f2930807b914330dc0b6104899851634428
Syed Ahmad Kumail Zaidi <kumail.zaidi708@gmail.com>
2025-10-13 08:59:35 +0000

    thk  to isy sahi kro ta k me dobara deploy kr sko

--- next.config.ts
+++ next.config.ts
@@ -32,6 +32,7 @@ const nextConfig: NextConfig = {
   },
   env: {
     NEXT_PUBLIC_EXCHANGE_RATE_API_KEY: process.env.NEXT_PUBLIC_EXCHANGE_RATE_API_KEY,
+    GEMINI_API_KEY: process.env.GEMINI_API_KEY,
   }
 };
 
