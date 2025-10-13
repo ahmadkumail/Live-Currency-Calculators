f4b1de5e4902e00551dcf927a70d8b20270873a4
Syed Ahmad Kumail Zaidi <kumail.zaidi708@gmail.com>
2025-10-13 08:30:04 +0000

    ab site bhi to chao error jo de rhi h

--- next.config.ts
+++ next.config.ts
@@ -30,6 +30,9 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  env: {
+    NEXT_PUBLIC_EXCHANGE_RATE_API_KEY: process.env.NEXT_PUBLIC_EXCHANGE_RATE_API_KEY,
+  }
 };
 
 export default nextConfig;
